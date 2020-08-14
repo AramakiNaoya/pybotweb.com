a9b66aeea576af6c821a7ef8946df4eb970f856a
AramakiNaoya <naoya.aramaki0629@gmail.com>
2020-08-15 00:24:28 +0900

    test

--- pybotweb.py
+++ pybotweb.py
@@ -1,15 +1,14 @@
 from bottle import route, run, template, request
-from datetime import datetime
 from pybot import pybot
 
 @route("/hello")
 def hello():
-    return template("pybot_template.tpl", input_text="", output_text="")
+    return template("index.html", input_text="", output_text="")
 
 @route("/hello", method="post")
 def do_hello():
     input_text = request.forms.input_text
     output_text = pybot(input_text)
-    return template("pybot_template.tpl", input_text=input_text, output_text=output_text)
+    return template("index.html", input_text=input_text, output_text=output_text)
 
-run(host="localhost", port=8080, debug=True)
+run(host="localhost", port=8080, debug=True, reloader=True)
